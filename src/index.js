dd76423d70f3c79f935a5991da4b0e8e36e638db
Chilly712 <chillinthestar@outlook.com>
2022-04-19 23:15:27 -0500

    Add `React 18.0.0` createRoot render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+const useReactLatest = false;
+if (useReactLatest) {
+  createRoot(document.getElementById("root")).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+  );
+} else {
+  ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById("root")
+  );
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
