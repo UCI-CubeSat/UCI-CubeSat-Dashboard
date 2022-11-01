4ce0f9a36181c157aa3c28d1fbfb8f120209ebae
Gaby P <gabrielle.a.palar@gmail.com>
2022-11-01 14:34:45 -0700

    Adding new fields to email sign up + aesthetic changes

--- src/component/page/emailSignup.tsx
+++ src/component/page/emailSignup.tsx
@@ -1,35 +1,137 @@
-import React from "react";
-import {
-  FormControl,
-  FormHelperText,
-  InputLabel
-} from "@mui/material";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import MailIcon from '@mui/icons-material/Mail';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import OutlinedInput from "@mui/material/OutlinedInput";
 
+// based off of code from siriwatknp https://github.com/mui/material-ui/blob/v5.10.12/docs/data/material/getting-started/templates/sign-up/SignUp.tsx
 
-const EmailSignup: React.FC<{}> = () => {
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" align="center" {...props}>
+      {'Copyright © '}
+      UCI CubeSat
+      {' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignUp() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
 
   return (
-    <div className="SignUp"
-      style={{
-        display: "flex", justifyContent: "center",
-        alignItems: "center", height: "50vh"
-      }}>
-      <FormControl sx={{ width: "80%" }}>
-        <InputLabel htmlFor="my-input"
-          style={{ color: "white" }}>
-          Email Address
-        </InputLabel>
-        <OutlinedInput id="my-input"
-          aria-describedby="my-helper-text"
-          style={{ color: "white" }} />
-        <FormHelperText id="my-helper-text" style={{ color: "white" }}>
-          {/* eslint-disable-next-line react/no-unescaped-entities */}
-          We'll never share your email.
-        </FormHelperText>
-      </FormControl>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+            <MailIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            CubeSat Mailing List Sign Up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  sx={{ input: { color: "white" } }}
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  sx={{ input: { color: "white" } }}
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  sx={{ input: { color: "white" } }}
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  sx={{ input: { color: "white" } }}
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive updates about upcoming satellites via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
   );
-};
-export default EmailSignup;
+}
