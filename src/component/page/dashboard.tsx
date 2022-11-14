c74ff0bb36cea99d544b9a5e0d6a42515f3758e5
neverlandiz <2katherinehsu@gmail.com>
2022-11-13 21:38:08 -0800

    Update Recharts Graph

--- src/component/page/dashboard.tsx
+++ src/component/page/dashboard.tsx
@@ -1,5 +1,8 @@
 import moment from "moment";
 import React, { useEffect, useState } from "react";
+import { LineChart, Line } from 'recharts';
+import { CartesianGrid, XAxis, YAxis } from 'recharts';
+const data = [{name: 'A', uv: 100, pv: 2400, amt: 2400}, {name: 'B', uv: 200, pv: 2400, amt: 2400}, {name: 'C', uv: 300, pv: 2400, amt: 2400}, {name: 'D', uv: 200, pv: 2400, amt: 2400}, {name: 'E', uv: 100, pv: 2400, amt: 2400}];
 
 const Dashboard: React.FC<{}> = () => {
   const [time, setTime] = useState("");
@@ -22,8 +25,15 @@ const Dashboard: React.FC<{}> = () => {
       <p className="time">
         {time}
       </p>
+      <LineChart width={600} height={300} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
     </React.Fragment>
   );
+
 };
 
 export default Dashboard;
