b8e5a0e2cbe6782181f61c2202a5bae4ef82bfa3
Rishab Dalai <rishabdalai213@gmail.com>
2023-03-04 17:20:14 -0800

    Correctly define props for battery component

--- src/component/Dashboard/Battery.tsx
+++ src/component/Dashboard/Battery.tsx
@@ -1,12 +1,12 @@
 import { ParsedLog } from "@/model/log"
 import { transformNumberToDate } from "@/util/transform"
 import { Typography } from "@mui/material"
+import Box from '@mui/material/Box'
+import Tab from '@mui/material/Tab'
+import Tabs from '@mui/material/Tabs'
+import * as React from 'react'
 import TableComp from "../TableComp/Table"
 import TimeSeriesGraph from "../TimeSeriesGraph/TimeSeriesGraph"
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import * as React from 'react';
 
 // This constant defines an object that contains the headers for the battery logs.
 const BatteryHeaders = {
@@ -17,49 +17,53 @@ const BatteryHeaders = {
     batteryVoltage: { title: "Battery Voltage" },
     batteryCurrent: { title: "Battery Current" },
     batteryTemp: { title: "Battery Temp" },
-    chargingVoltage: { title: "Charging Voltage"},
+    chargingVoltage: { title: "Charging Voltage" },
     // isCharging: {
     //     title: "Is Charing",
     //     }
 } as const;
 
 // This type defines the props for the Battery component.
-type Props = {
+type TabPanelProps = {
     logs: Array<ParsedLog>,
     children?: React.ReactNode,
     index: number,
     value: number
 };
 
+type BatteryProps = {
+    logs: Array<ParsedLog>,
+}
+
 // This function returns the accessibility properties for the specified tab index.
 function a11yProps(index: number) {
     return {
-      id: `battery-tab-${index}`,
-      'aria-controls': `battery-tabpanel-${index}`,
+        id: `battery-tab-${index}`,
+        'aria-controls': `battery-tabpanel-${index}`,
     };
-  };
+};
 
 // This function defines the TabPanel component.
-function TabPanel(props: Props) {
-const { children, index, value } = props;
-return (
-    <div
-    role="tabpanel"
-    hidden={value !== index}
-    id={`battery-tabpanel-${index}`}
-    aria-labelledby={`battery-tab-${index}`}
-    >
-    {value === index && (
-        <Box sx={{ p: 0 }}>
-        <Typography>{children}</Typography>
-        </Box>
-    )}
-    </div>
-);
+function TabPanel(props: TabPanelProps) {
+    const { children, index, value } = props;
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`battery-tabpanel-${index}`}
+            aria-labelledby={`battery-tab-${index}`}
+        >
+            {value === index && (
+                <Box sx={{ p: 0 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
 // This component should show batteryVoltage, batteryCurrent, batteryTemp, chargingVoltage, and isCharging
-export default function Battery(props: Props) {
+export default function Battery(props: BatteryProps) {
     const { logs } = props
     const [value, setValue] = React.useState(0);
 
@@ -67,167 +71,167 @@ export default function Battery(props: Props) {
     const handleChange = (
         event: React.SyntheticEvent,
         newValue: number
-        ) => {
-      setValue(newValue);
+    ) => {
+        setValue(newValue);
     };
 
     return (
         <div
-        style={{
-            width: "100%",
-            padding: "20px",
-        }}
-    >
-        <Typography
-            variant="h4"
-            gutterBottom
-        >
-            Battery
-        </Typography>
-        <div
-        style={{
-            width: "100%",
-            padding: "10px",
-            backgroundColor: "white"
-        }}
-    >
-        <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
-            <Tabs value={value} onChange={handleChange} aria-label="BatteryTabs" sx={{
-                bgcolor: "white",
-                marginTop: "20px",
-                padding: "15px",
+            style={{
                 width: "100%",
-                borderRadius: "5px"
-                }}>
-            <Tab label="Voltage" {...a11yProps(0)} />
-            <Tab label="Current" {...a11yProps(1)} />
-            <Tab label="Temperature" {...a11yProps(2)} />
-            <Tab label="Is Charging" {...a11yProps(3)} />
-            <Tab label="Charging Voltage" {...a11yProps(4)} />
-            <Tab label="Battery Table" {...a11yProps(5)} />
-            </Tabs>
-        </Box>
-                
-        <TabPanel value={value} index={0} logs={logs}>
-                <TimeSeriesGraph
-                    title="Battery Voltage"
-                    style={{
-                        marginTop: "20px",
-                        height: "400px",
-                        width: "100%",
-                        backgroundColor: "white",
-                        padding: "20px",
-                        borderRadius: "5px"
-                    }}
-                    timeSeriesData={[
-                        {
-                            name: "Voltage",
-                            color: "#8884d8",
-                            data: logs.map(log => ({ time: log.timestamp, value: log.batteryVoltage }))
-                        },
-                    ]}
-                />
-        </TabPanel>
-        <TabPanel value={value} index={1} logs={logs}>
-            <TimeSeriesGraph
-                    title="Battery Current"
-                    style={{
-                        marginTop: "20px",
-                        height: "400px",
-                        width: "100%",
-                        backgroundColor: "white",
-                        padding: "20px",
-                        borderRadius: "5px"
-                    }}
-                    timeSeriesData={[
-                        {
-                            name: "Current",
-                            color: "#82ca9d",
-                            data: logs.map(log => ({ time: log.timestamp, value: log.batteryCurrent }))
-                        }
-                    ]}
-                />
-        </TabPanel>
-        <TabPanel value={value} index={2} logs={logs}>
-            <TimeSeriesGraph
-                    title="Battery Temperature"
-                    style={{
-                        marginTop: "20px",
-                        height: "400px",
-                        width: "100%",
-                        backgroundColor: "white",
-                        padding: "20px",
-                        borderRadius: "5px"
-                    }}
-                    timeSeriesData={[
-                        {
-                            name: "Temperature",
-                            color: "#db8f4d",
-                            data: logs.map(log => ({ time: log.timestamp, value: log.batteryTemp }))
-                        }
-                    ]}
-                />
-        </TabPanel>
+                padding: "20px",
+            }}
+        >
+            <Typography
+                variant="h4"
+                gutterBottom
+            >
+                Battery
+            </Typography>
+            <div
+                style={{
+                    width: "100%",
+                    padding: "10px",
+                    backgroundColor: "white"
+                }}
+            >
+                <Box sx={{ width: '100%' }}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <Tabs value={value} onChange={handleChange} aria-label="BatteryTabs" sx={{
+                            bgcolor: "white",
+                            marginTop: "20px",
+                            padding: "15px",
+                            width: "100%",
+                            borderRadius: "5px"
+                        }}>
+                            <Tab label="Voltage" {...a11yProps(0)} />
+                            <Tab label="Current" {...a11yProps(1)} />
+                            <Tab label="Temperature" {...a11yProps(2)} />
+                            <Tab label="Is Charging" {...a11yProps(3)} />
+                            <Tab label="Charging Voltage" {...a11yProps(4)} />
+                            <Tab label="Battery Table" {...a11yProps(5)} />
+                        </Tabs>
+                    </Box>
 
-        <TabPanel value={value} index={3} logs={logs}>
-            <TimeSeriesGraph
-                    title="Is Charging"
-                    style={{
-                        marginTop: "20px",
-                        height: "400px",
-                        width: "100%",
-                        backgroundColor: "white",
-                        padding: "20px",
-                        borderRadius: "5px"
-                    }}
-                    timeSeriesData={[
-                        {
-                            name: "Boolean",
-                            color: "#48CAD3",
-                            data: logs.map(log => ({ time: log.timestamp, value: log.isCharging ? 0 : 1}))
-                        }
-                    ]}
-                />
-        </TabPanel>
+                    <TabPanel value={value} index={0} logs={logs}>
+                        <TimeSeriesGraph
+                            title="Battery Voltage"
+                            style={{
+                                marginTop: "20px",
+                                height: "400px",
+                                width: "100%",
+                                backgroundColor: "white",
+                                padding: "20px",
+                                borderRadius: "5px"
+                            }}
+                            timeSeriesData={[
+                                {
+                                    name: "Voltage",
+                                    color: "#8884d8",
+                                    data: logs.map(log => ({ time: log.timestamp, value: log.batteryVoltage }))
+                                },
+                            ]}
+                        />
+                    </TabPanel>
+                    <TabPanel value={value} index={1} logs={logs}>
+                        <TimeSeriesGraph
+                            title="Battery Current"
+                            style={{
+                                marginTop: "20px",
+                                height: "400px",
+                                width: "100%",
+                                backgroundColor: "white",
+                                padding: "20px",
+                                borderRadius: "5px"
+                            }}
+                            timeSeriesData={[
+                                {
+                                    name: "Current",
+                                    color: "#82ca9d",
+                                    data: logs.map(log => ({ time: log.timestamp, value: log.batteryCurrent }))
+                                }
+                            ]}
+                        />
+                    </TabPanel>
+                    <TabPanel value={value} index={2} logs={logs}>
+                        <TimeSeriesGraph
+                            title="Battery Temperature"
+                            style={{
+                                marginTop: "20px",
+                                height: "400px",
+                                width: "100%",
+                                backgroundColor: "white",
+                                padding: "20px",
+                                borderRadius: "5px"
+                            }}
+                            timeSeriesData={[
+                                {
+                                    name: "Temperature",
+                                    color: "#db8f4d",
+                                    data: logs.map(log => ({ time: log.timestamp, value: log.batteryTemp }))
+                                }
+                            ]}
+                        />
+                    </TabPanel>
 
-        <TabPanel value={value} index={4} logs={logs}>
-            <TimeSeriesGraph
-                    title="Charging Voltage"
-                    style={{
-                        marginTop: "20px",
-                        height: "400px",
-                        width: "100%",
-                        backgroundColor: "white",
-                        padding: "20px",
-                        borderRadius: "5px"
-                    }}
-                    timeSeriesData={[
-                        {
-                            name: "Voltage",
-                            color: "#5273de",
-                            data: logs.map(log => ({ time: log.timestamp, value: log.chargingVoltage }))
-                        }
-                    ]}
-                />
-        </TabPanel>
-        <TabPanel value={value} index={5} logs={logs}>
-            <TableComp
-                    id="Battery"
-                    tableHeaders={BatteryHeaders}
-                    data={logs}
-                    style={{
-                        marginTop: "20px",
-                        height: "400px",
-                        width: "100%",
-                        backgroundColor: "white",
-                        padding: "20px",
-                        borderRadius: "5px"
-                    }}
-                />
-        </TabPanel>
-        </Box>
-        </div>
+                    <TabPanel value={value} index={3} logs={logs}>
+                        <TimeSeriesGraph
+                            title="Is Charging"
+                            style={{
+                                marginTop: "20px",
+                                height: "400px",
+                                width: "100%",
+                                backgroundColor: "white",
+                                padding: "20px",
+                                borderRadius: "5px"
+                            }}
+                            timeSeriesData={[
+                                {
+                                    name: "Boolean",
+                                    color: "#48CAD3",
+                                    data: logs.map(log => ({ time: log.timestamp, value: log.isCharging ? 0 : 1 }))
+                                }
+                            ]}
+                        />
+                    </TabPanel>
+
+                    <TabPanel value={value} index={4} logs={logs}>
+                        <TimeSeriesGraph
+                            title="Charging Voltage"
+                            style={{
+                                marginTop: "20px",
+                                height: "400px",
+                                width: "100%",
+                                backgroundColor: "white",
+                                padding: "20px",
+                                borderRadius: "5px"
+                            }}
+                            timeSeriesData={[
+                                {
+                                    name: "Voltage",
+                                    color: "#5273de",
+                                    data: logs.map(log => ({ time: log.timestamp, value: log.chargingVoltage }))
+                                }
+                            ]}
+                        />
+                    </TabPanel>
+                    <TabPanel value={value} index={5} logs={logs}>
+                        <TableComp
+                            id="Battery"
+                            tableHeaders={BatteryHeaders}
+                            data={logs}
+                            style={{
+                                marginTop: "20px",
+                                height: "400px",
+                                width: "100%",
+                                backgroundColor: "white",
+                                padding: "20px",
+                                borderRadius: "5px"
+                            }}
+                        />
+                    </TabPanel>
+                </Box>
+            </div>
         </div>
     );
 }
